d891e1a39d34279ac4906c809a431046fdf1232a
GrowingRick <liziranzr@gmail.com>
2020-05-28 14:23:04 +0800

    tololist完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,106 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入待办" class="my_ipt"
+      :value="inputValue"
+      @change="handleChange($event)" />
+    <a-button
+      type="primary"
+      @click="addItemToList"
+    >添加待办</a-button>
+    <a-list borderd :dataSource="todoList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 选择框 -->
+        <a-checkbox
+          :checked="item.done"
+          @change="(e) => {handleStatusChange(e, item.id)}"
+        >{{item.info}}</a-checkbox>
+        <a slot="actions" @click="handleDeleteItem(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer -->
+      <div slot="footer" class="footer">
+        <span>{{unDoneLength}}条剩余</span>
+        <a-button-group>
+          <a-button :type="viewKey === 'all' ? 'primary' : 'default'" @click="changeList('all')">全部</a-button>
+          <a-button :type="viewKey === 'undone' ? 'primary' : 'default'" @click="changeList('undone')">未完成</a-button>
+          <a-button :type="viewKey === 'done' ? 'primary' : 'default'" @click="changeList('done')">已完成</a-button>
+        </a-button-group>
+        <a @click="clean">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {}
+  },
+  created() {
+    this.$store.dispatch('getList')
+  },
+  computed: {
+    ...mapState(['inputValue', 'viewKey']),
+    ...mapGetters(['unDoneLength', 'todoList'])
+  },
+  methods: {
+    handleChange(e) {
+      this.$store.commit('setInputValue', e.target.value)
+    },
+    // 添加待办
+    addItemToList() {
+      if (this.inputValue.trim() === '') {
+        return this.$message.warning('输入内容不能为空!')
+      }
+      this.$store.commit('addItem')
+    },
+    // 删除待办
+    handleDeleteItem(id) {
+      this.$store.commit('deleteItem', id)
+    },
+    // 复选框
+    handleStatusChange(e, id) {
+      // e.target.checked
+      const param = {
+        id: id,
+        status: e.target.checked
+      }
+      this.$store.commit('changeStatus', param)
+    },
+    // 清除已完成
+    clean() {
+      this.$store.commit('cleanDone')
+    },
+    changeList(str) {
+      this.$store.commit('changeViewKey', str)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 20px;
+  width: 600px;
+  margin: 0 auto;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,81 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [], // 所有的任务列表
+    inputValue: '',
+    nextId: 4,
+    viewKey: 'all'
   },
   mutations: {
+    initList(state, list) {
+      state.list = list
+    },
+    setInputValue(state, val) {
+      state.inputValue = val
+    },
+    // 添加列表
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    // 删除待办
+    deleteItem(state, id) {
+      state.list = state.list.filter(item => {
+        return item.id !== id
+      })
+    },
+    changeStatus(state, param) {
+      state.list.forEach(item => {
+        if (item.id === param.id) {
+          item.done = param.status
+        }
+      })
+    },
+    // 清除已完成
+    cleanDone(state) {
+      state.list = state.list.filter(item => item.done === false)
+    },
+    // 修改视图
+    changeViewKey(state, key) {
+      state.viewKey = key
+    }
   },
   actions: {
+    getList(context) {
+      axios.get('./list.json').then(({ data }) => {
+        context.commit('initList', data)
+        console.log(context)
+      })
+    }
   },
-  modules: {
+  getters: {
+    // 统计未完成的条数
+    unDoneLength(state) {
+      return state.list.filter(item => {
+        return item.done === false
+      }).length
+    },
+    // 获取各个视图列表
+    todoList(state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      } else if (state.viewKey === 'undone') {
+        return state.list.filter(item => item.done === false)
+      } else if (state.viewKey === 'done') {
+        return state.list.filter(item => item.done)
+      }
+      return state.list
+    }
   }
 })
